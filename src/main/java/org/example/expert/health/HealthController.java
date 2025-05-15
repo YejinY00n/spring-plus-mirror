bb5508f3793d2d643199f1e8330bab07942833f1
YejinY00n <yoon17710@gmail.com>
2025-05-15 13:27:05 +0900

    Comment: 주석 추가

--- src/main/java/org/example/expert/health/HealthController.java
+++ src/main/java/org/example/expert/health/HealthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// Lv12: Health Check API
 @Controller
 public class HealthController {
   @GetMapping("/health")
